7ac6ad3e16b7ac610eb465b28ac25c6b22c3bd6c
caleb-james-smith <caleb_smith2@baylor.edu>
2019-01-15 14:41:31 -0600

    calculate kinetic energy

--- tracking-intro/kinematics.py
+++ tracking-intro/kinematics.py
@@ -1,11 +1,22 @@
 import DataFormats.FWLite as fwlite
 import ROOT
+import math
 
 events = fwlite.Events("tracks_and_vertices.root")
 tracks = fwlite.Handle("std::vector<reco::Track>")
+# The pion mass is 0.140 GeV (all masses in CMSSW are in GeV). 
+pionMass = 0.140
 
 for i, event in enumerate(events):
     event.getByLabel("generalTracks", tracks)
     for track in tracks.product():
-        print track.pt(), track.p(), track.px(), track.py(), track.pz()
-    if i > 100: break
+        # kinetic energy
+        # KE = p^2 / (2m)
+        #kineticEnergy = ( track.p() ** 2 ) / ( 2 * pionMass )
+        # E^2 = p^2c^2 + m^2c^4
+        E = math.sqrt( track.p() ** 2 + pionMass ** 2 ) 
+        # KE = E - mc^2
+        KE = E - pionMass
+        print track.pt(), track.p(), track.px(), track.py(), track.pz(),
+        print E, KE
+    if i > 10: break
